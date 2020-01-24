e5350a1048c06ba748ddf3b8df5c4910bdb547e7
Joshua Alexander Cajina Duarte <joshua.cajina@mobilesuitcase.com>
2020-01-24 12:32:24 -0600

    Agregar entidad GroupMembers

---
+++ CommunityAwakens.Domain/Entities/GroupMembers.cs
@@ -0,0 +1,14 @@
+﻿using CommunityAwakens.Domain.Common;
+using System;
+
+namespace CommunityAwakens.Domain.Entities
+{
+    public class GroupMembers : IAuditableEntity
+    {
+        public int Id { get; set; }
+        public int GroupId { get; set; }
+        public int MemberId { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime ModificationDate { get; set; }
+    }
+}
