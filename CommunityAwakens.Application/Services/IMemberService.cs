a8b297cbf5442d1ecb9afc054ddc743156a8613b
Joshua Alexander Cajina Duarte <joshua.cajina@mobilesuitcase.com>
2020-01-24 13:57:45 -0600

    Agregar búsqueda por grupo y id

--- CommunityAwakens.Application/Services/IMemberService.cs
+++ CommunityAwakens.Application/Services/IMemberService.cs
@@ -6,6 +6,9 @@ namespace CommunityAwakens.Application.Services
 {
     public interface IMemberService
     {
+        IEnumerable<Member> GetAllMembersByGroup(int groupId);
         Task<IEnumerable<Member>> GetAllMembersByGroupAsync(int groupId);
+        Member GetMemberById(int memberId);
+        Task<Member> GetMemberByIdAsync(int memberId);
     }
 }
