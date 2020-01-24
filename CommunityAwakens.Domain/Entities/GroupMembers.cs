9a6ff16100728fbcaf58aa4c1b6a0995b8d75581
Joshua Alexander Cajina Duarte <joshua.cajina@mobilesuitcase.com>
2020-01-24 13:56:47 -0600

    Cambiar id por objeto de referencia

--- CommunityAwakens.Domain/Entities/GroupMembers.cs
+++ CommunityAwakens.Domain/Entities/GroupMembers.cs
@@ -1,13 +1,14 @@
 ﻿using CommunityAwakens.Domain.Common;
 using System;
+using System.Collections.Generic;
 
 namespace CommunityAwakens.Domain.Entities
 {
     public class GroupMembers : IAuditableEntity
     {
         public int Id { get; set; }
-        public int GroupId { get; set; }
-        public int MemberId { get; set; }
+        public Group Group { get; set; }
+        public IEnumerable<Member> Members { get; set; }
         public DateTime CreationDate { get; set; }
         public DateTime ModificationDate { get; set; }
     }
