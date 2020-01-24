91f0be2827ae8327e58f784f4c6ffdaa38402ef3
Joshua Alexander Cajina Duarte <joshua.cajina@mobilesuitcase.com>
2020-01-24 12:29:32 -0600

    Agregar scaffold de servicio de miembros

---
+++ CommunityAwakens.Application/Services/IMemberService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CommunityAwakens.Domain.Entities;
+
+namespace CommunityAwakens.Application.Services
+{
+    public interface IMemberService
+    {
+        Task<IEnumerable<Member>> GetAllMembersByGroupAsync(int groupId);
+    }
+}
